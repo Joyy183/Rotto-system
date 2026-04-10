c8b2b98e7077ddd6dd0cea38a8a6b91d2bc930e6
Joyy183 <farahhussein852@gmail.com>
2026-04-10 22:20:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use((err, req, res, _next) => {
 });
 
 // ── MongoDB + start ───────────────────────────────────────────────────────
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 mongoose
   .connect(process.env.MONGO_URI, {
